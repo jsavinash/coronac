193f0bfefeafada504ac8c7551c1f8826400be91
Avinash <avinash@Avinashs-MacBook-Pro.local>
2020-08-24 22:03:24 +0530

    build

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jsavinash/sampleproject",
+    url="https://github.com/jsavinash/coronac",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
